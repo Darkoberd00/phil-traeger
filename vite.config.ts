4a08f0fc335780f1942a5f1591f6fb4d62fd9a82
Phil Träger <traeger@ihf.rwth-aachen.de>
2023-10-13 13:53:43 +0200

    fix: :wrench: change Address

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://phil-traeger.dev/',
+  base: 'http://144.76.43.131:3000/',
   plugins: [
     vue(),
   ],
