a7200da38b765a10555ea65625dc4655d2e67e06
Phil Träger <traeger@ihf.rwth-aachen.de>
2023-10-16 17:56:01 +0200

    fix: :wrench: testing blur

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 import { RouterLink } from 'vue-router'
 import { useI18n } from 'vue-i18n'
 import { ref } from 'vue'
@@ -7,6 +8,7 @@ const { t } = useI18n()
 const { changeLang, isDE } = useLangStore()
 const name = import.meta.env.VITE_NAME
 const eevee_count = import.meta.env.VITE_EEVEE_COUNT
+const dropdown = ref<HTMLElement>()
 let showEevee = ref(false)
 let count = 0
 
@@ -18,30 +20,39 @@ function toggle(): void {
   count = 0
   showEevee.value = !showEevee.value
 }
+
+function removeFocus(): void {
+  console.log(dropdown.value)
+  setTimeout(() => {
+    console.log("test")
+    dropdown.value?.blur()
+  }, 300)
+
+}
 </script>
 
 <template>
   <div class="rounded-box mt-4 shadow-xl max-md:mx-4 md:mx-auto md:w-[50%]">
     <div class="navbar rounded-box bg-base-300">
       <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn-ghost btn-circle btn">
+        <div ref="dropdown" class="dropdown">
+          <label tabindex="0" class="btn btn-circle btn-ghost">
             <v-icon name="hi-menu-alt-2" scale="1.5" />
           </label>
           <ul
             tabindex="0"
-            class="dropdown-content menu rounded-box menu-sm z-[1] mt-3 w-52 space-y-1 bg-base-300 p-2 shadow-2xl"
+            class="menu dropdown-content rounded-box menu-sm z-[1] mt-3 w-52 space-y-1 bg-base-300 p-2 shadow-2xl"
           >
             <li>
-              <RouterLink to="/" exactActiveClass="active">{{ t('navbar.about') }}</RouterLink>
+              <RouterLink @click="removeFocus" to="/" exactActiveClass="active">{{ t('navbar.about') }}</RouterLink>
             </li>
             <li>
-              <RouterLink to="/contact" exactActiveClass="active">{{
+              <RouterLink @click="removeFocus" to="/contact" exactActiveClass="active">{{
                 t('navbar.contact')
               }}</RouterLink>
             </li>
             <li>
-              <RouterLink to="/project" exactActiveClass="active">{{
+              <RouterLink @click="removeFocus" to="/project" exactActiveClass="active">{{
                 t('navbar.project')
               }}</RouterLink>
             </li>
@@ -49,15 +60,15 @@ function toggle(): void {
         </div>
       </div>
       <div class="navbar-center">
-        <div @click="toggle" v-if="!showEevee" class="btn-ghost btn text-xl normal-case">
+        <div @click="toggle" v-if="!showEevee" class="btn btn-ghost text-xl normal-case">
           <div>{{ name }}</div>
         </div>
-        <div @click="toggle" v-else class="btn-ghost btn text-xl normal-case">
+        <div @click="toggle" v-else class="btn btn-ghost text-xl normal-case">
           <v-icon name="pi-eevee" scale="2" />
         </div>
       </div>
       <div class="navbar-end">
-        <div @click="changeLang" class="btn-ghost btn">
+        <div @click="changeLang" class="btn btn-ghost">
           <div>
             <img
               v-if="isDE()"
